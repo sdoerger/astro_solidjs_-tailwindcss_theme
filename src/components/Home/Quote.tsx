ee34db6439ed211a04703626f1976b24c65c07e1
Stefan <doerger.stefan@gmail.com>
2022-07-03 14:21:51 +0200

    Fix type #main

--- src/components/Home/Quote.tsx
+++ src/components/Home/Quote.tsx
@@ -3,7 +3,7 @@ export default function (props: { title: string; subtitle: string }) {
     <>
       <div class="h-32 md:h-40"></div>
       <p class="font-serif text-4xl">
-        <span class="text-gray-400">{props.subtitle}</span>
+        <span class="text-gray-400">{props.title}</span>
 
         <span class="text-gray-600 pl-2">{props.subtitle}</span>
       </p>
